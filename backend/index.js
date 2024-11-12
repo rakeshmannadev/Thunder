841e24aa2935bbd5d6e1f895ef7353c128b26245
rakesh <rakeshmanna762@gmail,com>
2024-11-12 10:24:47 +0530

    backend:user routes & controller initialized

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.routes.js";
+
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is started at port: ${PORT}`);
