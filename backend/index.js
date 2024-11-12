966b446a4c97dadcd33a0370879e2b46bbde071c
rakesh <rakeshmanna762@gmail,com>
2024-11-12 18:58:15 +0530

    fileupload middleware implemented with express-fileupload

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "express";
+
 import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
 
 import userRoutes from "./routes/user.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
 import connectDb from "./db/connectDb.js";
 
 dotenv.config();
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
 app.use(clerkMiddleware()); // it attach the user in req-> req.auth
-
-const PORT = process.env.PORT || 3000;
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "temp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024,
+    },
+  })
+);
 
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/admin", adminRoutes);
@@ -27,6 +39,8 @@ app.use((err, req, res, next) => {
         : err.message,
   });
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is started at port: ${PORT}`);
