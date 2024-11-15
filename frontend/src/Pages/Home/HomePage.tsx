3ae6e7f1bbfe11daa885323a108fb4fa19f472a7
rakesh <rakeshmanna762@gmail,com>
2024-11-15 22:28:52 +0530

    frontend:Homepage designed

--- frontend/src/Pages/Home/HomePage.tsx
+++ frontend/src/Pages/Home/HomePage.tsx
@@ -1,14 +1,51 @@
 import Header from "@/components/Header";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import FeaturedSection from "./components/FeaturedSection";
+import SectionGrid from "./components/SectionGrid";
 
 
 
 const HomePage = () => {
-
+const madeForYouSongs = [
+  {
+    _id: "34234",
+    title: "Kesariya",
+    imageUrl: "/Kesariya.jpg",
+    artist: "Arijit Singh",
+    album: "Bramhastra",
+    audioUrl: "string",
+    duration: 123,
+    
+  },
+]
+const trendingSongs = [
+  {
+    _id: "34234",
+    title: "Kesariya",
+    imageUrl: "/Kesariya.jpg",
+    artist: "Arijit Singh",
+    album: "Bramhastra",
+    audioUrl: "string",
+    duration: 123,
+    
+  },
+]
+const isLoading=false;
   return (
-    <main className="rounded-md overflow-hidden" >
-      <Header />
-     
-    </main>
+    <main className='rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900'>
+			<Header />
+			<ScrollArea className='h-[calc(100vh-180px)]'>
+				<div className='p-4 sm:p-6'>
+					<h1 className='text-2xl sm:text-3xl font-bold mb-6'>Good afternoon</h1>
+					<FeaturedSection />
+
+					<div className='space-y-8'>
+						<SectionGrid title='Made For You' songs={madeForYouSongs} isLoading={isLoading} />
+						<SectionGrid title='Trending' songs={trendingSongs} isLoading={isLoading} />
+					</div>
+				</div>
+			</ScrollArea>
+		</main>
   );
 };
 
