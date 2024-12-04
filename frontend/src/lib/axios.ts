267b6b330a0da8c977a449c26c9a5ae2da6f0a5f
rakesh <rakeshmanna762@gmail,com>
2024-12-04 20:30:55 +0530

    socket & axios url changed

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api/v1"
+      : "/api/v1",
 
   withCredentials: true,
 });
--- frontend/src/store/useSocketStore.ts
+++ frontend/src/store/useSocketStore.ts
@@ -61,7 +61,7 @@ interface SocketState {
 
 const baseURL =
   import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
-console.log(baseURL);
+
 const useSocketStore = create<SocketState>((set, get) => ({
   socket: null,
   isLoading: false,
